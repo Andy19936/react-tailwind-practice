680ffcdcfe52f1d374158f7559c27d8ba0845b99
Andy19936 <andy19936@gmail.com>
2021-09-05 22:33:37 -0700

    tailwind scaffolding complete

---
+++ src/components/Content.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <>
+      <div className="menu-card">
+        <img
+          src="https://ketopots.com/wp-content/uploads/2019/08/5a-keto-egg-muffins-square.jpg"
+          alt="egg"
+          className="h-full rounded mb-20 shadow"
+        />
+        <div className="center-content"></div>
+        <h2 className="text-2xl mb-2">Egg Muffins</h2>
+        <p className="mb-2">Crispy, delicious, and nutritious!</p>
+        <span>$16</span>
+      </div>
+      <div className="menu-card">
+        <img
+          src="https://www.glutenfreefollowme.com/wp-content/uploads/2019/05/YlNTlZxGRiyUD9DqB4ysaA.jpg"
+          alt="egg"
+          className="h-full rounded mb-20 shadow"
+        />
+        <div className="center-content"></div>
+        <h2 className="text-2xl mb-2">Avocado Eggs </h2>
+        <p className="mb-2">Tasty, guilt free, and a work of art!</p>
+        <span>$20</span>
+      </div>
+    </>
+  );
+};
+
+export default Content;
